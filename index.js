da6424aaed786618c30ab69df8f0d2ff8af6bbd7
Levladik <vladislavtyshenko@yandex.ru>
2024-08-23 16:22:13 +0400

    fix parallax

--- index.js
+++ index.js
@@ -1,115 +1,110 @@
-window.onload = function () {
+window.onload = function() {
   let timeline = anime.timeline({
-    easing: "linear",
+    easing: 'linear'
   });
 
   anime({
-    targets: ".front-1",
-    easing: "linear",
+    targets: '.front-1',
+    easing: 'linear',
     duration: 30000,
-    rotate: "1turn",
-    direction: "alternate",
-    loop: true,
+    rotate: '1turn',
+    direction: 'alternate',
+    loop: true
   });
 
   anime({
-    targets: ".front-2",
-    easing: "linear",
+    targets: '.front-2',
+    easing: 'linear',
     duration: 80000,
     translateX: [0, -200],
     translateY: [0, 500],
-    rotate: "-360",
-    direction: "alternate",
-    loop: true,
+    rotate: '-360',
+    direction: 'alternate',
+    loop: true
   });
 
   anime({
-    targets: ".front-3",
-    easing: "linear",
+    targets: '.front-3',
+    easing: 'linear',
     duration: 100000,
     translateX: [0, 200],
-    rotate: "-360",
-    direction: "alternate",
-    loop: true,
+    rotate: '-360',
+    direction: 'alternate',
+    loop: true
   });
 
   anime({
-    targets: ".front-4",
-    easing: "linear",
+    targets: '.front-4',
+    easing: 'linear',
     duration: 100000,
     translateX: [0, 200],
-    rotate: "360",
-    direction: "alternate",
-    loop: true,
+    rotate: '360',
+    direction: 'alternate',
+    loop: true
   });
 
   const float = () => {
     anime({
-      targets: ".starship",
-      easing: "easeInOutSine",
+      targets: '.starship',
+      easing: 'easeInOutSine',
       duration: 5000,
       translateY: [0, 10],
-      rotate: "2",
-      direction: "alternate",
-      loop: true,
+      rotate: '2',
+      direction: 'alternate',
+      loop: true
     });
   };
 
   timeline
     .add({
-      targets: ".overlay",
+      targets: '.overlay',
       opacity: [1, 0],
       duration: 1000,
-      complete: function (anim) {
-        document.querySelector(".overlay").remove();
-      },
+      complete: function(anim) {
+        document.querySelector('.overlay').remove();
+      }
     })
     .add({
-      targets: ".starship",
+      targets: '.starship',
       translateY: [-500, 0],
       translateX: [800, 0],
       scale: [0, 1],
-      easing: "easeOutQuint",
+      easing: 'easeOutQuint',
       delay: 1000,
       duration: 2000,
     })
-    .add(
-      {
-        targets: ".flame",
-        scale: 0.5,
-        opacity: [1, 0],
-        easing: "easeInElastic(3, 0.3)",
-        duration: 800,
-      },
-      "-=1900"
-    );
+    .add({
+      targets: '.flame',
+      scale: .5,
+      opacity: [1, 0],
+      easing: 'easeInElastic(3, 0.3)',
+      duration: 800
+    }, '-=1900');
 
   timeline.finished.then(float);
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const layers = document.querySelectorAll(".parallax");
+  const layers = document.querySelectorAll('.parallax');
 
-    function parallax(e) {
-      const x = (window.innerWidth - e.pageX * 2) / 100;
-      const y = (window.innerHeight - e.pageY * 2) / 100;
+  function parallax(e) {
+    const x = (window.innerWidth - e.pageX * 2) / 100;
+    const y = (window.innerHeight - e.pageY * 2) / 100;
 
-      layers.forEach((layer) => {
-        const speed = layer.getAttribute("data-speed");
-        const xOffset = (x * speed).toFixed(2);
-        const yOffset = (y * speed).toFixed(2);
+    layers.forEach(layer => {
+      const speed = layer.getAttribute('data-speed');
+      const xOffset = (x * speed).toFixed(2);
+      const yOffset = (y * speed).toFixed(2);
 
-        layer.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
-      });
-    }
+      layer.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
+    });
+  }
 
-    document.addEventListener("mousemove", parallax);
+  document.addEventListener('mousemove', parallax);
 
-    function initializePositions() {
-      layers.forEach((layer) => {
-        layer.style.transform = "translate(0px, 0px)";
-      });
-    }
+  function initializePositions() {
+    layers.forEach(layer => {
+      layer.style.transform = 'translate(0px, 0px)';
+    });
+  }
 
-    initializePositions();
-  });
+  initializePositions();
 };
